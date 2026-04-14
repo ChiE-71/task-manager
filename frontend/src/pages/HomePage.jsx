2e8e3b35f18fc46af20df823935d5752395bf2f3
Chua Qi Yi <qiyi1213@gmail.com>
2026-04-15 00:37:02 +0800

    Develop 2nd iteration UI for HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
+import Tasks from "./Tasks";
+
 function HomePage() {
   return (
-    <div className="login-page flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h2 className="text-black">Home Page</h2>
+    <div className="bg-background text-font">
+      <div className="login-page flex flex-col items-center  min-h-screen">
+        <div className="my-10">
+          <h1 className="font-heading text-4xl font-semibold">TASK MANAGER</h1>
+        </div>
+        <div className="bg-font w-full h-0.5" />
+        <div className="flex flex-col items-center gap-5 py-10">
+          <Tasks />
+        </div>
+      </div>
     </div>
   );
 }
